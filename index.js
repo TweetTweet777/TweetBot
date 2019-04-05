b09c75ffab8c6994fc13598eb42481ccca7400f8
TweetTweet777 <48974347+TweetTweet777@users.noreply.github.com>
2019-04-06 11:07:15 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json")
+client.on("ready", () => {
+    console.log("Ready!")
+    client.user.setActivity("tping")
+});
+client.on("message", async message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+if (message.content.startsWith(config.prefix + "ping")) {
+const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+}});
+client.login(config.token);
