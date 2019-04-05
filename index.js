63098c01721a8055e69905b9b3675978281924c0
TweetTweet777 <48974347+TweetTweet777@users.noreply.github.com>
2019-04-06 12:46:57 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json")
-const Math = require("mathjs");
+const math = require("mathjs");
 client.on("ready", () => {
     console.log("Ready!")
     client.user.setActivity("tping")
@@ -13,21 +13,33 @@ client.on("message", async message => {
 if (message.content.startsWith(config.prefix + "ping")) {
 const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-}});
-if (message.content.startsWith(config.prefix + "math")) {
+} if (message.content.startsWith(config.prefix + "math")) {
     if (!args[0]) return message.channel.send("Please input a valid equation or calculation!");
     let resp;
-    try{
-        resp = math.eval(args.join(" "));
+    try {
+      resp = math.eval(args.join(" "));
     } catch (e) {
-        return message.channel.send("Please input a valid equation or calculation!");
+      return message.channel.send("Please input a valid equation or calculation!");
     }
-    const mathembed = new Discord.RichEmbed()
-    .setColour(0xFFA500)
-    .setTitle(message.author.username, message.author.avatarURL)
-    .setDescription("Math Calculation!")
-    .addField("Input", `\`\`\`js\n${args.join("")}\`\`\``)
-    .addField("Output", `\`\`\`js\n${resp}\`\`\``)
-    message.channel.send(mathembed)
-}
+const mathembed = new Discord.RichEmbed()
+.setColor(0xFFA500)
+.setTitle("Math Calculation")
+.setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
+.addField("Input", `\`\`\`js\n${args.join("")}\`\`\``)
+.addField("Output", `\`\`\`js\n${resp}\`\`\``)
+message.channel.send(mathembed)
+
+} if (message.content.startsWith(config.prefix + "uptime")) {
+let totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+let uptime = `${days} days, ${hours} hours and ${minutes} minutes`;
+message.channel.send({embed:{
+    color:0xFFA500,
+    description: uptime
+}});
+}});
 client.login(process.env.token);
