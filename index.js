7cc180c06ae2b7ac612ae01a2a88ef4b09f5718e
TweetTweet777 <48974347+TweetTweet777@users.noreply.github.com>
2019-04-06 11:08:30 +1300

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ if (message.content.startsWith(config.prefix + "ping")) {
 const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }});
-client.login(config.token);
+client.login(process.env.token);
