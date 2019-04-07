fe7b20c21c151f0be18fcb5e174d4168bb533954
TweetTweet777 <48974347+TweetTweet777@users.noreply.github.com>
2019-04-07 18:21:13 +1200

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,10 @@ message.channel.send({embed:{
           name: "Uptime",
           value: "Shows the uptime of the bot"
         },
+        {
+            name: "Say",
+            value: "Say something that gets embeded"
+        }
       ],
       timestamp: new Date(),
       footer: {
@@ -54,5 +58,27 @@ message.channel.send({embed:{
       }
     } 
   });
-}});
+} if (message.content.startsWith(config.prefix + "say")) {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+    // To get the "message" itself we join the `args` back into a string with spaces:
+    
+      const sayMessage = args.join(" ");
+
+      let servIcon = message.guild.iconURL;
+      let esayEmbed = new Discord.RichEmbed()
+      .setTitle("Say")
+      .setColor("0xFFA500")
+      .setThumbnail(client.user.avatarURL)
+      .setDescription(`Said by ${message.author}`)
+      .addField("Message", `${sayMessage}`)
+      .setTimestamp();
+
+      const esayMessage = args.join(" ");
+      // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+      message.delete().catch(O_o=>{});
+      // And we get the bot to say the thing:
+
+      message.channel.send(esayEmbed);
+  }
+});
 client.login(process.env.token);
